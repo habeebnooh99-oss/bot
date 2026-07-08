b9ae62e195db7502fab80d6fb887e97310942f32
habeebnooh99-oss <habeebnooh99@gmail.com>
2026-07-08 14:58:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -283,8 +283,9 @@ async def general_callback_handler(update: Update, context: ContextTypes.DEFAULT
     elif data == "dep_global":
         text = "🌍 **الشحن للدول العربية والأجنبية:**\n\nنوفر طرق دفع متعددة تناسب بلدك.\n📥 يرجى التواصل مع الإدارة مباشرة وإرسال اسم بلدك ليتم تزويدك بطرق التحويل المتاحة لك فوراً.\n\n💬 التواصل مع الإدارة:\nتليجرام : @htb1b"
         await query.edit_message_text(text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔙 رجوع", callback_data="back_to_dep")]]))
-    elif data == "main_menu":
-         await query.edit_message_text("🏠 أهلاً بك في القائمة الرئيسية:", reply_markup=get_main_keyboard(user_id))
+    if data == "main_menu":
+        await query.edit_message_text("🏠 أهلاً بك في القائمة الرئيسية:", reply_markup=get_main_keyboard(update.effective_user.id))
+        return
 
     # 2. تصفح العميل للمتجر
     elif data == "u_root":
